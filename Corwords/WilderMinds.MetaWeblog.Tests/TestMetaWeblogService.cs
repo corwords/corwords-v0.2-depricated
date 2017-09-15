cd17543deb5775c1bbfede3ea7d33269a9190675
jasongaylord <jason@jasongaylord.com>
2017-09-15 14:40:17 -0400

    Added in the WinderMinds.MetaWeblog stuff directly into this project as the latest package is not building in Github/AppVeyor.

---
+++ Corwords/WilderMinds.MetaWeblog.Tests/TestMetaWeblogService.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WilderMinds.MetaWeblog;
+
+namespace MetaWeblog.Tests
+{
+  public class TestMetaWeblogService : IMetaWeblogProvider
+  {
+    public int AddCategory(string key, string username, string password, NewCategory category)
+    {
+      return 1;
+    }
+
+    public string AddPost(string blogid, string username, string password, Post post, bool publish)
+    {
+      return "123";
+    }
+
+    public bool DeletePost(string key, string postid, string username, string password, bool publish)
+    {
+      return true;
+    }
+
+    public bool EditPost(string postid, string username, string password, Post post, bool publish)
+    {
+      return true;
+    }
+
+    public CategoryInfo[] GetCategories(string blogid, string username, string password)
+    {
+      return new CategoryInfo[]
+      {
+        new CategoryInfo() { categoryid = "1", title = "ASP.NET", htmlUrl = "/cats/aspnet" }
+      };
+    }
+
+    public Post GetPost(string postid, string username, string password)
+    {
+      return new Post()
+      {
+        postid = 1,
+        dateCreated = DateTime.UtcNow,
+        description = "<p>This post is a long post</p>",
+        permalink = "/123",
+        title = "This is a post",
+        userid = "swildermuth",
+        categories = new string[] { "usda" }
+      };
+    }
+
+    public Post[] GetRecentPosts(string blogid, string username, string password, int numberOfPosts)
+    {
+      throw new NotImplementedException();
+    }
+
+    public UserInfo GetUserInfo(string key, string username, string password)
+    {
+      return new UserInfo()
+      {
+        firstname = "Shawn",
+        lastname = "Wildermuth",
+        email = "me@us.com",
+        userid = "1"
+      };
+    }
+
+    public BlogInfo[] GetUsersBlogs(string key, string username, string password)
+    {
+      return new BlogInfo[]
+      {
+        new BlogInfo()
+        {
+          blogid = "1",
+          blogName = "Test Blog",
+          url = "http://foo.com"
+        }
+      };
+    }
+
+    public MediaObjectInfo NewMediaObject(string blogid, string username, string password, MediaObject mediaObject)
+    {
+      return new MediaObjectInfo();
+    }
+  }
+}
---
+++ Corwords/WilderMinds.MetaWeblog/MetaWeblogMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace WilderMinds.MetaWeblog
+{
+  public class MetaWeblogMiddleware
+  {
+    private ILogger _logger;
+    private readonly RequestDelegate _next;
+    private MetaWeblogService _service;
+    private string _urlEndpoint;
+
+    public MetaWeblogMiddleware(RequestDelegate next, ILoggerFactory loggerFactory, string urlEndpoint, MetaWeblogService service)
+    {
+      _next = next;
+      _logger = loggerFactory.CreateLogger<MetaWeblogMiddleware>(); ;
+      _urlEndpoint = urlEndpoint;
+      _service = service;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+      if (context.Request.Method == "POST" &&
+        context.Request.Path.StartsWithSegments(_urlEndpoint) && 
+        context.Request != null && 
+        context.Request.ContentType.ToLower().Contains("text/xml"))
+      {
+        context.Response.ContentType = "text/xml";
+        var rdr = new StreamReader(context.Request.Body);
+        var xml = rdr.ReadToEnd();
+        _logger.LogInformation($"Request XMLRPC: {xml}");
+        var result = _service.Invoke(xml);
+        _logger.LogInformation($"Result XMLRPC: {result}");
+        await context.Response.WriteAsync(result, Encoding.UTF8);
+        return;
+      }
+
+      // Continue On
+      await _next.Invoke(context);
+    }
+  }
+}
