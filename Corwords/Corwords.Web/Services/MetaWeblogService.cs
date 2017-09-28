cc674ea9c0cd36c1c6f04279b6fab96b28bb532b
jasongaylord <jason@jasongaylord.com>
2017-09-28 14:11:27 -0400

    Added a logincheck to each of the metaweblogservice calls.

--- Corwords/Corwords.Web/Services/MetaWeblogService.cs
+++ Corwords/Corwords.Web/Services/MetaWeblogService.cs
@@ -52,49 +52,55 @@ namespace Corwords.Web.Services
 
         public BlogInfo[] GetUsersBlogs(string key, string username, string password)
         {
-            if (!Login(username, password))
-                throw new UnauthorizedAccessException("The username and password combination supplied is invalid. Please check the credentials and try again.");
-
+            LoginCheck(username, password);
             return _blogManager.GetBlogs(username).Select(s => new BlogInfo() { blogid = s.BlogId.ToString(), blogName = s.Name, url = s.BaseUrl }).ToArray();
         }
 
         public Post GetPost(string postid, string username, string password)
         {
+            LoginCheck(username, password);
             throw new NotImplementedException();
         }
 
         public Post[] GetRecentPosts(string blogid, string username, string password, int numberOfPosts)
         {
+            LoginCheck(username, password);
             throw new NotImplementedException();
         }
 
         public string AddPost(string blogid, string username, string password, Post post, bool publish)
         {
+            LoginCheck(username, password);
             throw new NotImplementedException();
         }
 
         public bool DeletePost(string key, string postid, string username, string password, bool publish)
         {
+            LoginCheck(username, password);
             throw new NotImplementedException();
         }
 
         public bool EditPost(string postid, string username, string password, Post post, bool publish)
         {
+            LoginCheck(username, password);
             throw new NotImplementedException();
         }
 
         public CategoryInfo[] GetCategories(string blogid, string username, string password)
         {
+            LoginCheck(username, password);
             throw new NotImplementedException();
         }
 
         public MediaObjectInfo NewMediaObject(string blogid, string username, string password, MediaObject mediaObject)
         {
+            LoginCheck(username, password);
             throw new NotImplementedException();
         }
 
         public int AddCategory(string key, string username, string password, NewCategory category)
         {
+            LoginCheck(username, password);
             throw new NotImplementedException();
         }
 
@@ -104,6 +110,12 @@ namespace Corwords.Web.Services
             signin.Wait();
 
             return signin.Result.Succeeded;
+        }
+
+        private void LoginCheck(string username, string password)
+        {
+            if (!Login(username, password))
+                throw new UnauthorizedAccessException("The username and password combination supplied is invalid. Please check the credentials and try again.");
         }
     }
 }
