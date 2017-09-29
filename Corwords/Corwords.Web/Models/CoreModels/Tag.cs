191b34d0701b4d7c71746d248e995da31696f8dc
jasongaylord <jason@jasongaylord.com>
2017-09-29 00:00:29 -0400

    Added the Tag model.

---
+++ Corwords/Corwords.Web/Models/CoreModels/Tag.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Corwords.Web.Models.CoreModels
+{
+    public class Tag
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TagId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
